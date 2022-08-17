90328c4c93ee8792f24f4f86b2cdfe9cc360db55
chandresh <er.chandreshbhai@gmail.com>
2022-08-17 09:59:45 +0530

    aug 17th 2022 set up

--- scanner.py
+++ scanner.py
@@ -22,7 +22,7 @@ with open("token.json") as json_file:
 
 # from_date = datetime.
 # now().strftime('%Y-%m-%d')
-from_date = "2022-08-12"
+from_date = "2022-08-16"
 to_date = datetime.now().strftime('%Y-%m-%d')
 
 # actual
@@ -38,7 +38,7 @@ adx = 50
 min_candle_time = 60
 max_candle_time = 240
 capital = 10000
-frequency = 20
+frequency = 25
 volume_filter = 20000
 price_filter_min = 50
 price_filter_max = 50000
